297e354be925dc4c93d3c31cab176261bef0da74
Mikovich8 <122255149+Mikovich8@users.noreply.github.com>
2023-05-25 23:21:57 +0300

    Smirnov Mustrid rabota

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,75 +1,117 @@
+import tkinter as tk
+from tkinter.font import Font
 from tkinter import *
+from random import *
 
-# Funktsioon, mis kuvab Bahama lippu
-def show_bahamas_lipp():
-    canvas.delete("all")
-    canvas.create_rectangle(0, 0, 200, 133, fill="#00A5E3")
-    canvas.create_polygon(0, 0, 200, 0, 200, 44.3, 0, 44.3, fill="#FCD116")
-    canvas.create_polygon(0, 88.7, 200, 88.7, 200, 133, 0, 133, fill="#CE1126")
+root = tk.Tk()
+root.title("lippud")
+root.configure(background="white")
 
-# Funktsioon, mis kuvab Eesti lippu
-def show_eesti_lipp():
-    canvas.delete("all")
-    canvas.create_rectangle(0, 0, 200, 133, fill="#0072C6")
-    canvas.create_rectangle(0, 0, 66.6, 133, fill="#000000")
-    canvas.create_rectangle(66.6, 0, 133.3, 133, fill="#FFFFFF")
-    canvas.create_rectangle(133.3, 0, 200, 133, fill="#D50F25")
-
-# Funktsioon, mis kuvab Itaalia lippu
-def show_itaalia_lipp():
-    canvas.delete("all")
-    canvas.create_rectangle(0, 0, 200, 133, fill="#009246")
-    canvas.create_rectangle(0, 0, 66.6, 133, fill="#FFFFFF")
-    canvas.create_rectangle(66.6, 0, 133.3, 133, fill="#F1BF00")
-    canvas.create_rectangle(133.3, 0, 200, 133, fill="#CE2B37")
-
-# loo tkinter akna
-root = Tk()
-root.title("Lipud")
-
-# loo tkinter canvas (ristk¸lik)
-canvas = Canvas(root, width=200, height=133, bg="white")
+canvas = tk.Canvas(root, width=500, height=500, bg="white")
 canvas.pack()
 
-# loo tkinter nupud
-Button(root, text="Bahama saarte lipp", command=show_bahamas_lipp).pack()
-Button(root, text="Eesti riigi lipp", command=show_eesti_lipp).pack()
-Button(root, text="Itaalia riigi lipp", command=show_itaalia_lipp).pack()
+def show_bahamas_flag():
+    canvas.delete("all")
+    canvas.create_rectangle(30, 50, 300, 150, fill="cyan")
+    canvas.create_rectangle(30, 100, 300, 150, fill="yellow")
+    canvas.create_rectangle(30, 200, 300, 150, fill="cyan")
+    canvas.create_polygon(30, 50, 100, 125, 30, 200, fill="black")
 
-# k‰ivita tkinter akna s¸ndmuste ts¸kkel
-root.mainloop()
+def show_estonia_flag():
+    canvas.delete("all")
+    canvas.create_rectangle(0, 50, 300, 150, fill="Blue")
+    canvas.create_rectangle(0, 100, 300, 150, fill="Black")
+    canvas.create_rectangle(0, 200, 300, 150, fill="White")
+
+def show_lithuanian_flag():
+    canvas.delete("all")
+    canvas.create_rectangle(0, 50, 300, 150, fill="Yellow")
+    canvas.create_rectangle(0, 100, 300, 150, fill="Green")
+    canvas.create_rectangle(0, 200, 300, 150, fill="Red")
 
 
-def draw(canvas, x, y, width, height, num_squares, num_circles):
-    if num_squares == 0 and num_circles == 0:
-        return
-    elif num_squares > 0:
-        canvas.create_rectangle(x, y, x+width, y+height)        
-        draw(canvas, x+width, y, width, height, num_squares-1, num_circles)
-    elif num_circles > 0:
-        canvas.create_oval(x, y, x+width, y+height)
-        draw(canvas, x+width, y, width, height, num_squares, num_circles-1)
+def create_flag_button(text, command):
+    button_font = Font(family="Helvetica", size=14, weight="bold")
+    button = tk.Button(root, text=text, font=button_font, command=command)
+    button.pack(side=LEFT, padx=5, pady=10)
 
-def on_submit():
-    num_squares = int(square_entry.get())
-    num_circles = int(circle_entry.get())
-    draw(canvas, 0, 0, 50, 50, num_squares, num_circles)
+def show_malevaljak():
+    canvas.delete("all")
+    cell_size = 50
+    for row in range(8):
+        for col in range(8):
+            x1 = col * cell_size
+            y1 = row * cell_size
+            x2 = x1 + cell_size
+            y2 = y1 + cell_size
+            if (row + col) % 2 == 0:
+                canvas.create_rectangle(x1, y1, x2, y2, fill="white")
+            else:
+                canvas.create_rectangle(x1, y1, x2, y2, fill="black")
 
-root = Tk()
-canvas = Canvas(root, width=500, height=500)
-canvas.pack()
+def show_oval():
+    canvas.delete("all")
+    colors=["black",
+        "cyan",
+        "magenta",
+        "red",
+        "blue",
+        "gray",
+        "yellow",
+        "green",
+        "lightblue",
+        "pink",
+        "gold"]
+    x0 = 0
+    y0 = 0
+    x1 = 500
+    y1 = 500
+    p = 2
+    for i in range(55):
+        x0 += p
+        y0 += p
+        x1 -= p
+        y1 -= p
+        canvas.create_oval(x0, y0, x1, y1, fill=choice(colors))
 
-square_label = Label(root, text="Squares:")
-square_label.pack(side=LEFT)
-square_entry = Entry(root)
-square_entry.pack(side=LEFT)
 
-circle_label = Label(root, text="Circles:")
-circle_label.pack(side=LEFT)
-circle_entry = Entry(root)
-circle_entry.pack(side=LEFT)
 
-submit_button = Button(root, text="Draw", command=on_submit)
-submit_button.pack(side=LEFT)
+def show_valgusfloor():
+    canvas.delete("all")
+    canvas.create_line(60, 50, 100, 50, width=45, fill=("Red"))
+    canvas.create_line(60, 100, 100, 100, width=45, fill=("Orange"))
+    canvas.create_line(60, 150, 100, 150, width=45, fill=("Green"))
+    canvas.create_line(80, 180, 80, 300, width=8, fill=("Black"))
+    canvas.create_line(30, 310, 140, 310, width=4, fill=("Black"))
+    canvas.pack() 
+
+
+def show_ruutringis():
+    canvas.delete("all")
+    k=7
+    x0=0
+    y0=0
+    x1=550
+    y1=550
+    for i in range(k):
+        x0+=50
+        y0+=50
+        x1-=50
+        y1-=50
+        canvas.create_rectangle(x0,y0,x1,y1,width=1,outline="blue", fill="Red")
+        canvas.create_oval(x0,y0,x1,y1,width=1, outline="blue", fill="Yellow")
+    canvas.pack(row=0, column=0)
+
+
+
+
+create_flag_button("Bahama lipp", show_bahamas_flag)
+create_flag_button("Eesti riigi lipp", show_estonia_flag)
+create_flag_button("Leedu lipp", show_lithuanian_flag)
+create_flag_button("malev√§ljak", show_malevaljak)
+create_flag_button("mitmev√§rviline ovaalne", show_oval)
+create_flag_button("Valgusfoor", show_valgusfloor)
+create_flag_button("Ruut ringis", show_ruutringis)
+
 
 root.mainloop()
